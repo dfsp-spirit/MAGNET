48f1f3d373c1eb185155613eeca7b3afb71b8fa6
Tim Schäfer <ts+code@rcmd.org>
2020-05-05 15:01:08 +0200

    fix some issues and add sanity checks

--- MAGNET_Tool/Scripts/Pre_GWAS.sh
+++ MAGNET_Tool/Scripts/Pre_GWAS.sh
@@ -3,24 +3,57 @@
 # 11) Split into chunks of 5000 and Find the minor allele frequency of the SNPS	     #
 ######################################################################################
 
+LocSource=$(pwd)
+
+source /$LocSource/ConfigFiles/Tools.config
+source /$LocSource/ConfigFiles/Thresholds.config
+
+check_tool "PLINK" $PLINK
 
 
-awk '{print $2}' Subset_EUAIMS.bim >AllSNPs.txt
+data_files_name="Subset_EUAIMS"
+bim_file="$data_files_name.bim"
+bed_file="$data_files_name.bed"
 
 
-NSNPs=($(wc -l AllSNPs.txt)) #Total number of SNPs
+if [ ! -f AllSNPs.txt ]; then
 
-DivFiles=$[$NSNPs/$chunkSize] #Files after dividing into chunks of 5000
+		if [ ! -f "$bim_file" ]; then
+			echo "Missing input bim file '$bim_file'. Exiting."
+			exit 1
+		else
+			echo "Creating 'AllSNPs.txt' file from bim file '$bim_file'."
+			awk '{print $2}' $bim_file > AllSNPs.txt
+		fi
+
+else
+	echo "Using existing 'AllSNPs.txt' file. Delete it and re-run to avoid that."
+fi
+
+if [ ! -f "$bed_file" ]; then
+	echo "Missing input bed file '$bed_file'. Exiting."
+	exit 1
+fi
+
+NSNPs=$(cat AllSNPs.txt | wc -l) #Total number of SNPs
+echo "Found '$NSNPs' NSNPs. Using chunk size '$chunkSize'."
+
+DivFiles=$(expr $NSNPs / $chunkSize) #Files after dividing into chunks of 5000
 
 if [[($(expr $NSNPs % $chunkSize) -gt 1)]]; then #If there is a remainder existing after dividing it into chunks of 5000
 	NFiles=$(expr $DivFiles + 1) #If exists then add 1 to the number of main files
-	else Nfiles=$DivFiles #Else let the number as it is
+	echo "Remainder, NFiles is now '$NFiles' (DivFiles='$DivFiles')."
+else
+	Nfiles=$DivFiles #Else let the number as it is
+	echo "No remainder, NFiles is now '$NFiles' (DivFiles='$DivFiles')."
 fi
 
 
 digi=${#NFiles}
-
 x=$NFiles
+
+echo "DEBUG: digi='$digi', x='$x'"
+
 split -a $digi -l 5000 -d --additional-suffix=.txt AllSNPs.txt splitFiles
 
 
@@ -31,21 +64,20 @@ Files=($(ls splitFiles*.txt))
 
 for (( i=0; i<=$x; i++ ))
 do
-echo ${Files[i]}
-	if [[ -e ${Files[i]} ]]
-	then
-	$PLINK --bfile Subset_EUAIMS --extract ${Files[i]} --make-bed --out tmp"$i"
+	echo ${Files[i]}
+	if [[ -e ${Files[i]} ]]; then
+		$PLINK --bfile Subset_EUAIMS --extract ${Files[i]} --make-bed --out tmp"$i"
 	fi
-wait
+	wait
 done
 
 for (( i=0; i<=$x; i++ ))
 do
 	if [[ -e tmp"$i".bim ]]
 	then
-	echo ${Files[i]}
-	$PLINK --bfile tmp"$i" --recodeA --out Data_SNPfile"$i" 
-	rm tmp"$i"
+		echo ${Files[i]}
+		$PLINK --bfile tmp"$i" --recodeA --out Data_SNPfile"$i"
+		rm tmp"$i"
 	fi
 done
 
@@ -53,4 +85,3 @@ done
 echo -e "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- \n"
 
 echo -e "----------------------------------------------------------------Stage 2 Imputation completed successfully-------------------------------------------------------------------------- \n"
-
